3d5bd60b2d4924c20fef32fb4e1545ae948cfb8e
=Shirley Yin <sshirley.yin@gmail.com>
2016-08-07 13:24:29 -0700

    attempt at animating circles

--- js/program.js
+++ js/program.js
@@ -11,6 +11,31 @@ var unpressed = "#ffffff";
 var leftAttached = "false";
 var rightAttached = "false";
 
+// progressbar.js from http://progressbarjs.readthedocs.io/en/latest/
+var circleOutline = new ProgressBar.Circle(LH_btn, {
+    strokeWidth: 2,
+    easing: 'linear',
+    duration: 555.55556,
+    color: '#ffffff',
+    trailColor: '#eee',
+    trailWidth: 0.5,
+    svgStyle: null,
+    text: {
+        value: 'press f',  
+        style: {
+            color: '#787878',
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            padding: 0,
+            margin: 0,
+            transform: {
+                prefix: true,
+                value: 'translate(-50%, -50%)'
+            }
+        },
+    }
+});
 
 var playNote = function(hand) {
     var startTime = new Date().getTime();
@@ -21,10 +46,13 @@ var playNote = function(hand) {
         if (leftAttached) {
             $(document).off("keydown.leftNamespace");
             setColor("LH_btn", unpressed);
-            restoreText("LH_btn_text");
+            // restoreText("LH_btn_text");
             leftAttached = "false";
         }
 
+        circleOutline.animate(1.0, function() {
+            console.log('Animation has finished');
+        });
         playSound("left_hand_audio");
         console.log("triplet eighth " + new Date().getTime());
         $(document).on("keydown.leftNamespace", leftEventListener.bind(leftEventListener, startTime));
@@ -35,7 +63,7 @@ var playNote = function(hand) {
         if (rightAttached) {
             $(document).off("keydown.rightNamespace");
             setColor("RH_btn", unpressed);
-            restoreText("RH_btn_text");
+            // restoreText("RH_btn_text");
             rightAttached = "false";
         }
 
@@ -59,7 +87,7 @@ var leftEventListener = function(startTime) {
         var score = calculateScore(timeElapsed)
 
         setColor("LH_btn", score);
-        removeText("LH_btn_text");
+        // removeText("LH_btn_text");
     }
 };
 
@@ -74,7 +102,7 @@ var rightEventListener = function(startTime) {
         var score = calculateScore(timeElapsed)
 
         setColor("RH_btn", score);
-        removeText("RH_btn_text");
+        // removeText("RH_btn_text");
     }
 };
 
@@ -117,4 +145,4 @@ var restoreText = function (btn) {
 };
  
 var leftTimer = setInterval(function() { playNote("left") }, 555.55556);
-var rightTimer = setInterval(function() { playNote("right") }, 416.666667);
+// var rightTimer = setInterval(function() { playNote("right") }, 416.666667);
