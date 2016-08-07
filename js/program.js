3cf120ee29322179b05009ebdea797d2a4d2177d
=Shirley Yin <sshirley.yin@gmail.com>
2016-08-06 17:04:49 -0700

    a Mess

--- js/program.js
+++ js/program.js
@@ -1,68 +1,87 @@
 // constants: colours
 var miss = "#f24e4e";
-var bad = "#f57e7e";
-var ok = "#fef5f5";
-var good = "#d1dec5";
-var great = "#abc394";
+var bad = "#f79696";
+var ok = "#dee7d5";
+var good = "#abc394";
+var great = "#85a863";
 var unpressed = "#ffffff";
 
-document.addEventListener("keydown", function(event) {
+// global variables
+// state of left/right buttons, whether or not an event listener is attached
+var leftAttached = "false";
+var rightAttached = "false";
+
+
+var playNote = function(hand) {
+    var startTime = new Date().getTime();
+    
+    if (hand === "left") {
+
+        // remove previous event listeners
+        if (leftAttached) {
+            $(document).off("keydown", leftEventListener.bind(leftEventListener, startTime));
+            leftAttached = "false";
+        }
+
+        playSound("left_hand_audio");
+        console.log("triplet eighth");
+        $(document).on("keydown", leftEventListener.bind(leftEventListener, startTime));
+
+    } else if (hand === "right") {
+
+        playSound("right_hand_audio");
+        console.log("sixteenth");
+
+        // fill in event listener stuff
+
+    }
+
+
+}
+
+var leftEventListener = function(startTime) {
+    leftAttached = "true";
+
     if(event.keyCode == 70) {
-        console.log("Left was pressed");
-        setColor("LH_btn", miss);
-        removeText("LH_btn_text");
-
-        document.addEventListener("keyup", function onKeyup() {
-            document.removeEventListener("keyup", onKeyup);
-            setColor("LH_btn", unpressed);
-            restoreText("LH_btn_text");
-        }, false);
-
+        console.log("left was pressed");
+        var currentTime = new Date().getTime();
+        var timeElapsed = currentTime - startTime;
+        // calculateScore(timeElapsed, hand)
+        console.log(timeElapsed);
     }
-    else if(event.keyCode == 74) {
-        console.log("Right was pressed");
-        setColor("RH_btn", miss);
-        removeText("RH_btn_text");
-
-        document.addEventListener("keyup", function onKeyup() {
-            document.removeEventListener("keyup", onKeyup);
-            setColor("RH_btn", unpressed);
-            restoreText("RH_btn_text");
-        }, false);
-    }
-});
-
-
-setInterval(function() {
-    playSound("left_hand_audio");
-    console.log("triplet eighth");
-}, 555.55556);
-
-setInterval(function() {
-    console.log("sixteenth");
-    playSound("right_hand_audio");
-}, 416.666667);
-
-var calculateScore = function calculateScore() {
-    // TODO
 };
 
-var playSound = function playSound(sound_type) {
+
+
+var calculateScore = function (timeElapsed, hand) {
+    var score;
+
+
+
+
+    return score;
+};
+
+var playSound = function (sound_type) {
     var sound = document.getElementById(sound_type);
     sound.play()
 };
 
-var setColor = function setColor(btn, color) {
+var setColor = function (btn, color) {
     var property = document.getElementById(btn);
     property.style.backgroundColor = color;
 };
 
-var removeText = function removeText(btn) {
+var removeText = function (btn) {
     var property = document.getElementById(btn);
     property.style.display = "none";
 };
 
-var resoreText = function restoreText(btn) {
+var restoreText = function (btn) {
     var property = document.getElementById(btn);
     property.style.display = "inline";
 };
+
+// left should be 555.55556
+var leftTimer = setInterval(function() { playNote("left") }, 2000);
+// var rightTimer = setInterval(function() { playNote("right") }, 416.666667);
